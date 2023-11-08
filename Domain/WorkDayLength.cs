70a14744a528bdec739bdb8f8ad128fc1577d5ea
mykoza <48719773+mykoza@users.noreply.github.com>
2023-11-08 22:01:54 +0100

    Add validation to WorkDayLength and make it immutable

--- Domain/WorkDayLength.cs
+++ Domain/WorkDayLength.cs
@@ -4,13 +4,25 @@ public class WorkDayLength
 {
     public WorkDayLength(TimeSpan length)
     {
+        ValidateLengthTimeSpan(length);
+
         Length = length;
     }
 
     public WorkDayLength(int lengthInMinutes)
     {
+        ValidateLengthTimeSpan(TimeSpan.FromMinutes(lengthInMinutes));
+
         Length = TimeSpan.FromMinutes(lengthInMinutes);
     }
 
-    public TimeSpan Length { get; set; }   
+    private void ValidateLengthTimeSpan(TimeSpan length)
+    {
+        if (length <= TimeSpan.Zero || length >= TimeSpan.FromDays(1))
+        {
+            throw new ArgumentOutOfRangeException(nameof(length), "Length must be greater than 0 minutes and less than 24 hours.");
+        }
+    }
+
+    public TimeSpan Length { get; init; }   
 }
